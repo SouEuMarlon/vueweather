04f6cbd5d3429953b9b4ce6df91df07210b9ec4d
Marlon Menezes <marlon_menezes@outlook.com>
2022-07-09 19:26:28 -0300

    deleted unused components

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -20,20 +20,17 @@ export default {};
 .footer {
   @apply flex items-center justify-center w-full h-auto text-center pb-14 pt-44 pr-5 pl-5 self-end;
 }
-
 p {
   @apply text-white text-sm font-normal;
 }
 a {
   @apply text-white text-sm font-medium underline;
 }
-
 @media screen and (max-width: 1280px) {
   .footer {
     @apply pb-6 pt-10 pr-5 pl-5;
   }
 }
-
 @media screen and (max-width: 768px) {
   .footer {
     @apply pt-16;
